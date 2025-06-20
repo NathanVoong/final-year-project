fbfec0b013ce74f7988eda42f72746612c4bcf49
Nathan Voong <nathan.voong@clearscore.com>
2025-06-20 19:03:42 +0100

    FE-7: set up logic to have cards clickable and redirect the user
    FE-7

--- onboarding-app/src/pages/Home/components/Card.tsx
+++ onboarding-app/src/pages/Home/components/Card.tsx
@@ -1,14 +1,23 @@
 import React from "react";
 import "../styles/Home.css";
+import {Link} from "react-router-dom";
 
-export function Card({ children }: { children: React.ReactNode }) {
-    return <div className="card">{children}</div>;
+export function Card({ children, destination }: { children: React.ReactNode, destination: string }) {
+    return (
+        <Link to={destination} className="card-link">
+            <div className="card">{children}</div>
+        </Link>
+    );
 }
 
 export function CardContent({ children }: { children: React.ReactNode }) {
     return <div className="card-content">{children}</div>;
 }
 
-export function CardImage({ children }: { children: React.ReactNode }) {
-    return <div className="cardImage">{children}</div>;
+export function CardImage({ children, destination }: { children: React.ReactNode, destination: string }) {
+    return (
+        <Link to={destination} className="card-link">
+            <div className="cardImage">{children}</div>
+        </Link>
+    );
 }
