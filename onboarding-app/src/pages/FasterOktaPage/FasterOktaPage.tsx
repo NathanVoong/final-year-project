dc9f14a5a6ef103d20d4c5415d6c1bac0e220bd7
Nathan Voong <nathan.voong@clearscore.com>
2025-07-24 15:56:38 +0100

    FE-18: Add page and css for faster okta verification

--- onboarding-app/src/pages/FasterOktaPage/FasterOktaPage.tsx
+++ onboarding-app/src/pages/FasterOktaPage/FasterOktaPage.tsx
@@ -1,14 +1,45 @@
 import "./styles/FasterOktaPage.css";
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import OktaSettings from "../../assets/OktaSettings.png";
+import OktaBiometric from "../../assets/OktaBiometric.png";
 
 export default function FasterOktaPage() {
     return (
         <div>
             <section className="section">
-                <h1>Set up faster Okta verification</h1>
-                <SyntaxHighlighter language="typescript">
-                    {`const hello = world`}
-                </SyntaxHighlighter>
+                <h1>Setting up faster Okta verification</h1>
+                <p>This is a guide on how to setup faster Okta fingerprint verification instead of mobile 2FA.</p>
+                <ol>
+                    <li>
+                        On your <a href="https://clearscore.okta-emea.com/app/UserHome"
+                                   target="_blank"
+                                   rel="noopener noreferrer">Okta homepage</a>, click on your name at the top-left and click on Settings.
+                        <div>
+                            <div className="img-wrapper">
+                                <img
+                                    src={OktaSettings}
+                                    alt='jFrog account menu showing "Edit Profile"'
+                                    className="okta-settings"
+                                />
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        On the right panel <b>‘Security Methods’</b> scroll down until you find <b>‘Security Key or
+                        Biometric Authenticator‘</b> and then set it up.
+                        <div>
+                            <div className="img-wrapper">
+                                <img
+                                    src={OktaBiometric}
+                                    alt='jFrog account menu showing "Edit Profile"'
+                                    className="okta-biometric"
+                                />
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        Now instead of needing to authenticate Okta through your phone every time, it’ll be much faster just tapping your finger on the sensor.
+                    </li>
+                </ol>
             </section>
         </div>
     );
