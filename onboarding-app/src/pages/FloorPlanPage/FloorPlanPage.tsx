11bc548bd2c5da7428ed294b0958e9d5cfe5ff7e
Nathan Voong <nathan.voong@clearscore.com>
2025-07-29 11:50:34 +0100

    FE-26: Add content for floor plan page

--- onboarding-app/src/pages/FloorPlanPage/FloorPlanPage.tsx
+++ onboarding-app/src/pages/FloorPlanPage/FloorPlanPage.tsx
@@ -1,14 +1,52 @@
 import "./styles/FloorPlanPage.css";
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import groundFloorSmall from "./assets/groundFloorSmall.jpg";
+import groundFloorBig from "./assets/groundFloorBig.png";
+import firstFloor from "./assets/firstFloor.jpg";
+import secondFloor from "./assets/secondFloor.jpg";
+import thirdFloor from "./assets/thirdFloor.jpg";
 
 export default function FloorPlanPage() {
     return (
         <div>
             <section className="section">
                 <h1>Floor plan and meeting rooms</h1>
-                <SyntaxHighlighter language="typescript">
-                    {`const hello = world`}
-                </SyntaxHighlighter>
+                <h3>Ground Floor</h3>
+                <div>
+                    <img
+                        src={groundFloorSmall}
+                        alt='jFrog account menu showing "Edit Profile"'
+                        className="floor-plan-image"
+                    />
+                    <img
+                        src={groundFloorBig}
+                        alt='jFrog account menu showing "Edit Profile"'
+                        className="floor-plan-image"
+                    />
+                </div>
+                <h3>First Floor</h3>
+                <div>
+                    <img
+                        src={firstFloor}
+                        alt='jFrog account menu showing "Edit Profile"'
+                        className="floor-plan-image"
+                    />
+                </div>
+                <h3>Second Floor</h3>
+                <div>
+                    <img
+                        src={secondFloor}
+                        alt='jFrog account menu showing "Edit Profile"'
+                        className="floor-plan-image"
+                    />
+                </div>
+                <h3>Third Floor ( Canteen/eating space )</h3>
+                <div>
+                    <img
+                        src={thirdFloor}
+                        alt='jFrog account menu showing "Edit Profile"'
+                        className="floor-plan-image"
+                    />
+                </div>
             </section>
         </div>
     );
